5c6b4c1d0442859d7f22417752ed8a1d0e84bdd4
Nimo Axmed <axmednafisa@gmail.com>
2021-03-02 09:48:04 +0000

    added function for counting down from 25

--- timer/index.js
+++ timer/index.js
@@ -4,12 +4,12 @@ const seconds = document.getElementById("seconds")
 const stopStart = document.getElementById("stop-start")
 const reset = document.getElementById("reset")
 
-let minutes2 = 25
+let minutes2 = 25 //starting with 25 minutes
 let seconds2 = 0
 let extraMin= 0
 let extraSec = 0
 
-function showRightTime(){
+function showRightTime(){  //adding "extra" zeros
     if (minutes2 < 10){
       minutes.textContent = extraMin + "" + minutes2
     } else if (minutes2 >= 10){
@@ -24,3 +24,22 @@ function showRightTime(){
   }
   
 showRightTime()
+
+
+let timer = setInterval(myTimer, 1000)
+   
+  function myTimer() {
+    let reachFive = false
+    seconds2 -=1
+    if (seconds2 < 0){
+    seconds2 = 59
+    minutes2 -= 1
+    }
+       if (minutes2 === 0 && seconds2 === 0){
+   clearInterval(timer)
+ }
+     
+    
+showRightTime()
+
+}
