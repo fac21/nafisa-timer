ef12739513cf43a1c535a59965c9de53a12744a0
Nimo Axmed <axmednafisa@gmail.com>
2021-03-02 11:22:24 +0000

    added function for reset button

--- timer/README.md
+++ timer/README.md
@@ -6,3 +6,7 @@ Initially i  wrapped the minutes- 25 and seconds- 00 in two span elements. But o
 I left the styling pretty basic as i spent most of the time on the Javascript, the only question i did have though is about the measurement properties that are best used. I mostly see rem and em used because they're more responsive so does that mean we should avoid using pixels?
 
 ## Javascript explanation/notes
+
+From making a slideshow previously i decided to use the setInterval function because i knew that everything within the function it would take as an argument would run once, at every interval. Initially i didn't have a function to show the correct time e.g 25:0 was displaying instead of 25:00 but wanted to come back to this. I also found that the page was displaying/counting down from 24:58. I realised this was because  i needed to set the let variable to 25 as well as making the text content of the const variable equal to it outside the function rather inside.
+
+ From then on i didn't have any problems with decrementing the variable and using if statements to keep it in bounds of what i wanted. Moving on to counting down from 5 minutes once the interval was cleared is what i probably spent a lot of time on.
--- timer/index.js
+++ timer/index.js
@@ -40,24 +40,30 @@ let timer = setInterval(myTimer, 1000)
    clearInterval(timer)  //once 0 is reached stop the function
    reachedFive = true //changing the variable to true
       if (reachedFive){ 
-       minutes2 = 5 //so countdown from 5 minutes can start
+       minutes2 = 5 //so countdown from 5 minutes can now start
        seconds2 = 0
        let timer2 = setInterval(myFunction, 1000)
-       function myFunction(){
-         seconds2 -=1
-         if (seconds2 < 0){
-         seconds2 = 59
-         minutes2 -= 1
+         function myFunction(){
+          seconds2 -=1
+          if (seconds2 < 0){
+          seconds2 = 59
+          minutes2 -= 1
+        }
+          if (minutes2 === 0 && seconds2 === 0){ 
+          clearInterval(timer2) //stopping the function at 0 by clearing the interval
           }
-         if (minutes2 === 0 && seconds2 === 0){ 
-        clearInterval(timer2) //stopping the function at 0 by clearing the interval
-          }
-        showRightTime()
-          }
+         showRightTime()
+        }
  }
  }
      
     
 showRightTime() //updating text content of minutes and seconds with each change
-
 }
+
+reset.addEventListener("click", function(){
+    minutes2 = 25 //setting minutes back to 25
+    seconds2 = 0
+    showRightTime() //function to display this
+  timer; //calling variable that contains the setInterval function to countdown minutes and seconds
+  })
